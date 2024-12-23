03543c8b52ea8923d0ea62c21c23dfe59d120adb
User <sedikkhaled60@gmail.com>
2024-12-23 05:19:00 +0200

    upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'myapp';
+  isactive:boolean=true
+  ay7aga:number=10
+  value:number=100
+}
